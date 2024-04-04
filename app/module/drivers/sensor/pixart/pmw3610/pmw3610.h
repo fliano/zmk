66211bf97fd77429fe5a29953cdece6f9d947c8b
Willy Straussberger <willy.asbach@googlemail.com>
2024-04-04 18:19:32 +0200

    hopefully fixes includes

--- app/module/drivers/sensor/pixart/pmw3610/pmw3610.h
+++ app/module/drivers/sensor/pixart/pmw3610/pmw3610.h
@@ -9,12 +9,12 @@
  * @brief Header file for the pmw3610 driver.
  */
 
-#include <drivers/sensor.h>
+#include <zephyr/drivers/sensor.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-  enum pmw3610_attribute {
+enum pmw3610_attribute {
     /** Sensor CPI for both X and Y axes. */
     PMW3610_ATTR_CPI = SENSOR_ATTR_PRIV_START,
 
@@ -35,7 +35,7 @@ extern "C" {
 
     /** Sampling frequency time during REST3 mode [ms]. */
     PMW3610_ATTR_REST3_SAMPLE_TIME,
-  };
+};
 
 #ifdef __cplusplus
 }
