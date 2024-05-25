e435d65709dd384d2b27f43e788b122b1a31a6de
Willy Straussberger <willy.asbach@googlemail.com>
2024-05-25 13:38:10 +0200

    use old definitions

--- app/include/zmk/rgb_underglow/current_status.h
+++ app/include/zmk/rgb_underglow/current_status.h
@@ -8,18 +8,18 @@
 
 #include <zmk/rgb_underglow/rgb_underglow_base.h>
 
-int zmk_rgb_ug_cs_toggle(void);
-int zmk_rgb_ug_cs_get_state(bool *state);
-int zmk_rgb_ug_cs_on(void);
-int zmk_rgb_ug_cs_off(void);
-int zmk_rgb_ug_cs_cycle_effect(int direction);
-int zmk_rgb_ug_cs_calc_effect(int direction);
-int zmk_rgb_ug_cs_select_effect(int effect);
-struct zmk_led_hsb zmk_rgb_ug_cs_calc_hue(int direction);
-struct zmk_led_hsb zmk_rgb_ug_cs_calc_sat(int direction);
-struct zmk_led_hsb zmk_rgb_ug_cs_calc_brt(int direction);
-int zmk_rgb_ug_cs_change_hue(int direction);
-int zmk_rgb_ug_cs_change_sat(int direction);
-int zmk_rgb_ug_cs_change_brt(int direction);
-int zmk_rgb_ug_cs_change_spd(int direction);
-int zmk_rgb_ug_cs_set_hsb(struct zmk_led_hsb color);
+int zmk_rgb_underglow_toggle(void);
+int zmk_rgb_underglow_get_state(bool *state);
+int zmk_rgb_underglow_on(void);
+int zmk_rgb_underglow_off(void);
+int zmk_rgb_underglow_cycle_effect(int direction);
+int zmk_rgb_underglow_calc_effect(int direction);
+int zmk_rgb_underglow_select_effect(int effect);
+struct zmk_led_hsb zmk_rgb_underglow_calc_hue(int direction);
+struct zmk_led_hsb zmk_rgb_underglow_calc_sat(int direction);
+struct zmk_led_hsb zmk_rgb_underglow_calc_brt(int direction);
+int zmk_rgb_underglow_change_hue(int direction);
+int zmk_rgb_underglow_change_sat(int direction);
+int zmk_rgb_underglow_change_brt(int direction);
+int zmk_rgb_underglow_change_spd(int direction);
+int zmk_rgb_underglow_set_hsb(struct zmk_led_hsb color);
