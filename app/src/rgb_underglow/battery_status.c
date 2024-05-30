b8d1c6a455a41075f5250b10776da926511f73d6
Willy Straussberger <willy.asbach@googlemail.com>
2024-05-30 09:27:51 +0200

    forgot import

--- app/src/rgb_underglow/battery_status.c
+++ app/src/rgb_underglow/battery_status.c
@@ -8,6 +8,7 @@
 
 #include <zephyr/logging/log.h>
 
+#include <zmk/rgb_underglow/battery_status.h>
 #include <zmk/rgb_underglow/current_status.h>
 #include <zmk/rgb_underglow/rgb_underglow_base.h>
 #include <zmk/rgb_underglow/startup_mutex.h>
